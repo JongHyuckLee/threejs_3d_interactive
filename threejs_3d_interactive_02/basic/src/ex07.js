35b940e68bf670890d154e06cd89cc43941dbd49
JongHyuckLee <yarn add --dev --exact prettier>
2023-09-04 00:23:42 +0900

    안개(Fog)

---
+++ threejs_3d_interactive_02/basic/src/ex07.js
@@ -0,0 +1,75 @@
+import * as THREE from "three";
+
+export default function example() {
+  const canvas = document.querySelector("#three-canvas");
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+
+  const scene = new THREE.Scene();
+  scene.fog = new THREE.Fog("black", 3, 7);
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.y = 1;
+  camera.position.z = 5;
+  camera.lookAt(0, 0, 0);
+
+  camera.updateProjectionMatrix();
+  scene.add(camera);
+
+  const light = new THREE.DirectionalLight(0xffffff, 1);
+  light.position.x = 1;
+  light.position.y = 3;
+  light.position.z = 10;
+  scene.add(light);
+
+  //  mesh
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshStandardMaterial({
+    //   color: 0xff0000,
+    //   color: red,
+    color: "#ff0000",
+  });
+
+  const meshes = [];
+  for (let i = 0; i < 10; i++) {
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = Math.random() * 5 - 2.5;
+    mesh.position.z = Math.random() * 5 - 2.5;
+    scene.add(mesh);
+    meshes.push(mesh);
+  }
+
+  let time = Date.now();
+  // 그리기
+  function draw() {
+    const newTime = Date.now();
+    const deltaTime = newTime - time;
+    time = newTime;
+
+    meshes.forEach((item) => {
+      item.rotation.y += deltaTime / 1000;
+    });
+    renderer.render(scene, camera);
+    // window.requestAnimationFrame(draw);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    // updateProjectionMatrix 카메라 투영에 관련된 값에 변화가 있을 경우 실행해야 함.
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+
+  window.addEventListener("resize", setSize);
+  draw();
+}
