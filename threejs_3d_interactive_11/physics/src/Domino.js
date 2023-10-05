6d51f26382c17f7a779374887a244245f12ef493
JongHyuckLee <yarn add --dev --exact prettier>
2023-10-05 22:11:58 +0900

    도미노 만들기 3 - 레이캐스팅

--- threejs_3d_interactive_11/physics/src/Domino.js
+++ threejs_3d_interactive_11/physics/src/Domino.js
@@ -7,19 +7,21 @@ export class Domino {
     this.scene = info.scene;
     this.cannonWorld = info.cannonWorld;
 
+    this.index = info.index;
+
     this.width = info.width || 0.6;
     this.height = info.height || 1;
     this.depth = info.depth || 0.2;
 
     this.x = info.x || 0;
-    this.y = info.y || 0.5;
+    this.y = info.y || 0.6;
     this.z = info.z || 0;
 
     this.rotationY = info.rotationY || 0;
 
     info.gltfLoader.load("/models/domino.glb", (glb) => {
       this.modelMesh = glb.scene.children[0];
-      //   console.log(glb.scene.children[0]);
+      this.modelMesh.name = `${this.index}번 도미노`;
       this.modelMesh.castShadow = true;
       this.modelMesh.position.set(this.x, this.y, this.z);
       this.scene.add(this.modelMesh);
---
+++ threejs_3d_interactive_11/physics/src/ex11.js
@@ -0,0 +1,158 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as CANNON from "cannon-es";
+import { PreventDragClick } from "./PreventDragClick";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Domino } from "./Domino";
+
+// ----- 주제: 도미노 만들기 1 - glb 배치
+
+// cannon.js 문서
+// http://schteppe.github.io/cannon.js/docs/
+// 주의! https 아니고 http
+
+export default function example() {
+  // Renderer
+
+  const canvas = document.querySelector("#three-canvas");
+  const preventDragClick = new PreventDragClick(canvas);
+  const renderer = new THREE.WebGLRenderer({
+    canvas,
+    antialias: true,
+  });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  // Scene
+  const scene = new THREE.Scene();
+
+  // Camera
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.y = 1.5;
+  camera.position.z = 4;
+  scene.add(camera);
+
+  // Light
+  const ambientLight = new THREE.AmbientLight("white", 0.5);
+  scene.add(ambientLight);
+
+  const directionalLight = new THREE.DirectionalLight("white", 1);
+  directionalLight.position.x = 1;
+  directionalLight.position.z = 2;
+  directionalLight.castShadow = true;
+  scene.add(directionalLight);
+
+  // Controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  // Loader
+  const gltfLoader = new GLTFLoader();
+
+  //   Cannon(물리 엔진)
+  const cannonWorld = new CANNON.World();
+  cannonWorld.gravity.set(0, -9.8, 0);
+
+  // 성능을 위한 세팅
+  // cannonWorld.allowSleep = true;
+  // cannonWorld.broadphase = new CANNON.SAPBroadphase(cannonWorld);
+  // cannonWorld.broadphase = new CANNON.NaiveBroadphase(cannonWorld); // 기본값
+  // cannonWorld.broadphase = new CANNON.GridBroadphase(cannonWorld); // 구역을 나누어 테스트
+
+  // Contact Material
+  const defaultMaterial = new CANNON.Material("default");
+
+  const defaultContactMaterial = new CANNON.ContactMaterial(
+    defaultMaterial,
+    defaultMaterial,
+    { friction: 0.1, restitution: 0.6 }
+  );
+  cannonWorld.defaultContactMaterial = defaultContactMaterial;
+
+  const floorShape = new CANNON.Plane();
+  const floorBody = new CANNON.Body({
+    mass: 0,
+    position: new CANNON.Vec3(0, 0.14, 0),
+    shape: floorShape,
+    material: defaultMaterial,
+  });
+  floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI / 2);
+  cannonWorld.addBody(floorBody);
+
+  // Mesh
+  const floorMesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(100, 100),
+    new THREE.MeshStandardMaterial({
+      color: "slategray",
+    })
+  );
+
+  floorMesh.rotation.x = -Math.PI / 2;
+  floorMesh.receiveShadow = true;
+  scene.add(floorMesh);
+
+  // 도미노 생성
+  const dominos = [];
+  let domino = null;
+  for (let i = -3; i < 17; i++) {
+    domino = new Domino({
+      index: i,
+      scene,
+      cannonWorld,
+      gltfLoader,
+      z: -i,
+    });
+    dominos.push(domino);
+  }
+
+  // 그리기
+  const clock = new THREE.Clock();
+
+  function draw() {
+    const delta = clock.getDelta();
+
+    cannonWorld.step(1 / 60, delta, 3);
+    dominos.forEach((item) => {
+      if (item.cannonBody) {
+        item.modelMesh.position.copy(item.cannonBody.position);
+        item.modelMesh.quaternion.copy(item.cannonBody.quaternion);
+      }
+    });
+
+    renderer.render(scene, camera);
+    renderer.setAnimationLoop(draw);
+  }
+
+  function setSize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.render(scene, camera);
+  }
+  // Raycaster
+  const raycaster = new THREE.Raycaster();
+  const mouse = new THREE.Vector2();
+
+  function checkIntersects() {
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObjects(scene.children);
+    console.log(intersects[0].object.name);
+  }
+
+  const sound = new Audio("/sounds/boing.mp3");
+
+  // 이벤트
+  window.addEventListener("resize", setSize);
+  canvas.addEventListener("click", (e) => {
+    mouse.x = (e.clientX / canvas.clientWidth - 0.5) * 2;
+    mouse.y = -(e.clientY / canvas.clientHeight - 0.5) * 2;
+    checkIntersects();
+  });
+
+  draw();
+}
