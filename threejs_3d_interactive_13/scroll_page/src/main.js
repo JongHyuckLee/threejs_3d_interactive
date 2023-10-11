a8db75920bccdd724f9b49640bff52b7395c7f23
JongHyuckLee <yarn add --dev --exact prettier>
2023-10-11 22:16:14 +0900

    스크롤 페이지 만들기 2 - 기본 구조 잡기

--- threejs_3d_interactive_13/scroll_page/src/main.js
+++ threejs_3d_interactive_13/scroll_page/src/main.js
@@ -1,65 +1,70 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 
 // ----- 주제: 스크롤에 따라 움직이는 3D 페이지
 
 // Renderer
-const canvas = document.querySelector('#three-canvas');
+const canvas = document.querySelector("#three-canvas");
 const renderer = new THREE.WebGLRenderer({
-	canvas,
-	antialias: true
+  canvas,
+  antialias: true,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
 
 // Scene
 const scene = new THREE.Scene();
+// scene.background = new THREE.Color("white");
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
-	75,
-	window.innerWidth / window.innerHeight,
-	0.1,
-	1000
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
 );
 camera.position.y = 1.5;
 camera.position.z = 4;
 scene.add(camera);
 
 // Light
-const ambientLight = new THREE.AmbientLight('white', 0.5);
+const ambientLight = new THREE.AmbientLight("white", 3.5);
 scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight('white', 1);
-directionalLight.position.x = 1;
-directionalLight.position.z = 2;
-scene.add(directionalLight);
+const spotLight = new THREE.SpotLight("white", 0.6);
+spotLight.position.set(0, 150, 100);
+spotLight.castShadow = true;
+spotLight.shadow.mapSize.width = 1024;
+spotLight.shadow.mapSize.height = 1024;
+spotLight.shadow.camera.near = 1;
+spotLight.shadow.camera.far = 200;
+scene.add(spotLight);
 
 // Mesh
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({
-	color: 'seagreen'
-});
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const floorMesh = new THREE.Mesh(
+  new THREE.PlaneGeometry(100, 100),
+  new THREE.MeshStandardMaterial({ color: "white" })
+);
+floorMesh.rotation.x = -Math.PI / 2;
+scene.add(floorMesh);
 
 // 그리기
 const clock = new THREE.Clock();
 
 function draw() {
-	const delta = clock.getDelta();
+  const delta = clock.getDelta();
 
-	renderer.render(scene, camera);
-	renderer.setAnimationLoop(draw);
+  renderer.render(scene, camera);
+  renderer.setAnimationLoop(draw);
 }
 
 function setSize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.render(scene, camera);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
 }
 
 // 이벤트
-window.addEventListener('resize', setSize);
+window.addEventListener("resize", setSize);
 
 draw();
