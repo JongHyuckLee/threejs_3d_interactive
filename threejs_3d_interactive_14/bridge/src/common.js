91ae466c203c5a0d14e9ed1d389cfbcef987aed6
JongHyuckLee <yarn add --dev --exact prettier>
2023-10-14 02:32:13 +0900

    무대 설치 1

--- threejs_3d_interactive_14/bridge/src/common.js
+++ threejs_3d_interactive_14/bridge/src/common.js
@@ -1,4 +1,4 @@
-import { Scene } from "three";
+import { BoxGeometry, MeshPhongMaterial, Scene } from "three";
 
 export const cm1 = {
   canvas: document.querySelector("#three-canvas"),
@@ -7,4 +7,21 @@ export const cm1 = {
 
 export const cm2 = {
   backgroundColor: "#3e1322",
+  lightColor: "#ffe9ac",
+  floorColor: "#111",
+  pillarColor: "#071d28",
+};
+
+export const geo = {
+  floor: new BoxGeometry(200, 1, 200),
+  pillar: new BoxGeometry(5, 10, 5),
+};
+
+export const mat = {
+  floor: new MeshPhongMaterial({
+    color: cm2.floorColor,
+  }),
+  pillar: new MeshPhongMaterial({
+    color: cm2.pillarColor,
+  }),
 };
--- threejs_3d_interactive_14/bridge/src/main.js
+++ threejs_3d_interactive_14/bridge/src/main.js
@@ -1,6 +1,8 @@
 import { cm1, cm2 } from "./common";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Pillar } from "./Pillar";
+import { Floor } from "./Floor";
 
 // ----- 주제: The Bridge 게임 만들기
 
@@ -12,6 +14,8 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 cm1.scene.background = new THREE.Color(cm2.backgroundColor);
 // Camera
 const camera = new THREE.PerspectiveCamera(
@@ -20,31 +24,68 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.y = 1.5;
-camera.position.z = 4;
+camera.position.x = -4;
+camera.position.y = 19;
+camera.position.z = 14;
 cm1.scene.add(camera);
 
 // Light
-const ambientLight = new THREE.AmbientLight("white", 0.5);
+const ambientLight = new THREE.AmbientLight(cm2.lightColor, 0.8);
 cm1.scene.add(ambientLight);
 
-const directionalLight = new THREE.DirectionalLight("white", 1);
-directionalLight.position.x = 1;
-directionalLight.position.z = 2;
-cm1.scene.add(directionalLight);
+const spotLightDistance = 50;
+const spotLight1 = new THREE.SpotLight(cm2.lightColor, 100000);
+spotLight1.castShadow = true;
+const spotLight2 = spotLight1.clone();
+const spotLight3 = spotLight1.clone();
+const spotLight4 = spotLight1.clone();
+
+spotLight1.position.set(
+  -spotLightDistance,
+  spotLightDistance,
+  spotLightDistance
+);
+spotLight2.position.set(
+  spotLightDistance,
+  spotLightDistance,
+  spotLightDistance
+);
+spotLight3.position.set(
+  -spotLightDistance,
+  spotLightDistance,
+  -spotLightDistance
+);
+spotLight4.position.set(
+  spotLightDistance,
+  spotLightDistance,
+  -spotLightDistance
+);
+cm1.scene.add(spotLight1, spotLight2, spotLight3, spotLight4);
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
-// Mesh
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshStandardMaterial({
-  color: "seagreen",
+// 물체 만들기
+const glassUnitSize = 1.2;
+// 바닥
+const floor = new Floor({
+  name: "floor",
 });
-const mesh = new THREE.Mesh(geometry, material);
-cm1.scene.add(mesh);
+// 기둥
+const pillar1 = new Pillar({
+  name: "pillar",
+  x: 0,
+  y: 5.5,
+  z: -glassUnitSize * 12 - glassUnitSize / 2,
+});
 
+const pillar2 = new Pillar({
+  name: "pillar",
+  x: 0,
+  y: 5.5,
+  z: +glassUnitSize * 12 + glassUnitSize / 2,
+});
 // 그리기
 const clock = new THREE.Clock();
 
