8c17794b34b7ace94b684ba6bf41a39bb3d41ceb
JongHyuckLee <yarn add --dev --exact prettier>
2023-10-18 02:10:47 +0900

    물리 엔진 기본 설정

--- threejs_3d_interactive_14/bridge/src/common.js
+++ threejs_3d_interactive_14/bridge/src/common.js
@@ -1,9 +1,15 @@
+import { Material, World } from "cannon-es";
 import { BoxGeometry, MeshPhongMaterial, Scene, SphereGeometry } from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 export const cm1 = {
   scene: new Scene(),
   gltfLoader: new GLTFLoader(),
   mixer: undefined,
+  // cannon
+  world: new World(),
+  defaultMaterial: new Material("default"),
+  glassMaterial: new Material("glass"),
+  playerMaterial: new Material("player"),
 };
 
 export const cm2 = {
